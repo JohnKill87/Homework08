a79e925dc1c6c62c1bb5f567fb8d88d5a1466323
JohnKill87 <yankin.ilja2012@yandex.ru>
2023-04-25 19:17:33 +0300

    Решение Домашнего задания 8 (Массивы)

--- src/Main.java
+++ src/Main.java
@@ -9,68 +9,87 @@ public class Main {
     }
     public static void task1() {
         System.out.println("Задача 1");
-        int[] appleInBox = new int[3];
-        appleInBox[0] = 1;
-        appleInBox[1] = 2;
-        appleInBox[2] = 3;
-        System.out.println(appleInBox[0]);
-        System.out.println(appleInBox[1]);
-        System.out.println(appleInBox[2]);
+        int[] apple = new int[3];
+        apple[0] = 1;
+        apple[1] = 2;
+        apple[2] = 3;
+        System.out.println(apple[0]);
+        System.out.println(apple[1]);
+        System.out.println(apple[2]);
 
-        float[] moneyInBank = {1.57f, 7.654f, 9.986f};
-        System.out.println(moneyInBank[0]);
-        System.out.println(moneyInBank[1]);
-        System.out.println(moneyInBank[2]);
+        float[] money = {1.57f, 7.654f, 9.986f};
+        System.out.println(money[0]);
+        System.out.println(money[1]);
+        System.out.println(money[2]);
 
-        int[] brainInMyHead = {23, 14, 10, 12, 5, 0};
-        System.out.println(brainInMyHead[0]);
-        System.out.println(brainInMyHead[1]);
-        System.out.println(brainInMyHead[2]);
-        System.out.println(brainInMyHead[3]);
-        System.out.println(brainInMyHead[4]);
-        System.out.println(brainInMyHead[5]);
+        int[] brain = {23, 14, 10, 12, 5, 0};
+        System.out.println(brain[0]);
+        System.out.println(brain[1]);
+        System.out.println(brain[2]);
+        System.out.println(brain[3]);
+        System.out.println(brain[4]);
+        System.out.println(brain[5]);
     }
     public static void task2() {
         System.out.println("Задача 2");
-        int[] appleInBox = new int[3];
-        appleInBox[0] = 1;
-        appleInBox[1] = 2;
-        appleInBox[2] = 3;
-        System.out.println(Arrays.toString(appleInBox));
+        int[] apple = new int[3];
+        apple[0] = 1;
+        apple[1] = 2;
+        apple[2] = 3;
+        System.out.println(Arrays.toString(apple));
 
 
-        float[] moneyInBank = {1.57f, 7.654f, 9.986f};
-        System.out.println(Arrays.toString(moneyInBank));
+        double[] money = {1.57, 7.654, 9.986};
+        System.out.println(Arrays.toString(money));
 
 
-        int[] brainInMyHead = {23, 14, 10, 12, 5, 0};
-        System.out.println(Arrays.toString(brainInMyHead));
+        int[] brain = {23, 14, 10, 12, 5, 0};
+        System.out.println(Arrays.toString(brain));
     }
     public static void task3() {
         System.out.println("Задача 3");
-        int[] appleInBox = new int[3];
-        appleInBox[0] = 1;
-        appleInBox[1] = 2;
-        appleInBox[2] = 3;
-        System.out.println(appleInBox[2] + ", " + appleInBox[1] + ", " + appleInBox[0]);
+        int[] apple = new int[3];
+        apple[0] = 1;
+        apple[1] = 2;
+        apple[2] = 3;
+        for (int i = apple.length - 1; i >= 0; i--) {
+            System.out.print(apple[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        double[] money = {1.57, 7.654, 9.986};
+        for (int i = money.length - 1; i >= 0; i--) {
+            System.out.print(money[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
 
 
-        float[] moneyInBank = {1.57f, 7.654f, 9.986f};
-        System.out.println(moneyInBank[2] + ", " + moneyInBank[1] + ", " + moneyInBank[0]);
-
-
-        int[] brainInMyHead = {23, 14, 10, 12, 5, 0};
-        System.out.println(brainInMyHead[5] + ", " + brainInMyHead[4] + ", " + brainInMyHead[3] + ", " + brainInMyHead[2] + ", " + brainInMyHead[1] + ", " + brainInMyHead[0]);
+        int[] brain = {23, 14, 10, 12, 5, 0};
+        for (int i = brain.length - 1; i >= 0; i--) {
+            System.out.print(brain[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
     public static void task4() {
         System.out.println("Задача 4");
-        int[] appleInBox = new int[3];
-        appleInBox[0] = 1;
-        appleInBox[1] = 2;
-        appleInBox[2] = 3;
-        for (int i = 0; i < appleInBox.length; i+=2) {
-            appleInBox[i]++;
-            System.out.println(Arrays.toString(appleInBox));
+        int[] apple = new int[3];
+        apple[0] = 1;
+        apple[1] = 2;
+        apple[2] = 3;
+        for (int i = 0; i < apple.length; i++) {
+            if (apple[i] % 2 != 0) {
+                apple[i] += 1;
+            }
         }
+        System.out.println(Arrays.toString(apple));
     }
 }
